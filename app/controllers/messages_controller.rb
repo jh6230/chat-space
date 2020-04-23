3262f89590aaded80b15b456b58ed6863fc0c8c1
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2020-04-23 15:37:19 +0900

    create-controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,17 @@
 class MessagesController < ApplicationController
   
   def index
+    @messages = Message.all
   end
+
+  def create
+    @message = Message.create(message_params)
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:body, :image)
+  end
+
+
 end
