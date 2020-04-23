c940d7d83fb78efe791b62c3736050cbc0f19cad
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2020-04-23 21:14:49 +0900

    メッセージの表示
    画像投稿OK確認

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,7 +6,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
-  process resize_to_fit: [800, 800]
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
@@ -30,6 +29,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   # version :thumb do
+  process resize_to_fit: [800, 800]
   #   process resize_to_fit: [50, 50]
   # end
 
