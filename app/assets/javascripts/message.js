2af0cf9922374a329c9d5a09b6050487c23d88cc
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2020-04-28 12:44:31 +0900

    非同期通信の実装（提出１回目）

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,39 @@
 $(function(){
+  function buildHTML(message){
+    if (message.image){
+      var html = `<div class='message'>
+                    <div class='user-info'>
+                      <p class='user-name'>
+                        ${message.user_name}
+                      </p>
+                      <p class='post-time'>
+                        ${message.created_at}
+                      </p>
+                    </div>
+                      <p class='message-text'>
+                        ${message.body}
+                      <img class="lower-message__image" src = ${message.image} >
+                      </p>
+                    </div>`
+              return html;
+    }else{
+      var html = `<div class='message'>
+                   <div class='user-info'>
+                    <p class='user-name'>
+                    ${message.user_name}
+                    </p>
+                    <p class='post-time'>
+                    ${message.created_at}
+                    </p>
+                  </div>
+                    <p class='message-text'>
+                      ${message.body}
+                    </p>
+                  </div>`
+              return html;
+    };
+  }
+
   $('#new_message').on('submit', function(e){
   e.preventDefault();
   var formData = new FormData(this);
@@ -11,7 +46,15 @@ $(function(){
     processData:false,
     contentType: false
   })
-});
-
-
+  .done(function(data){
+      var html = buildHTML(data);
+      $('.chat-main__messages').append(html);
+      $('.chat-main__messages').animate({scrollTop: $('.chat-main__messages')[0].scrollHeight}); 
+      $('form')[0].reset();
+      $('.submit-btn').prop('disabled', false);
+  })
+  .fail(function(){
+    alert('メッセージを入力してください');
+  })
 })
+});
